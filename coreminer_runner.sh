5274f0a264bfa8184520babef3e8e916777b6cb6
Sasha Zezulinsky <alexander.zezulinsky@gmail.com>
2022-01-14 00:48:48 +0100

    coreminer runner bash script

---
+++ coreminer_runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LARGE_PAGES=""
+if [ -f /proc/sys/vm/nr_hugepages ]
+then
+    if [ $(cat /proc/sys/vm/nr_hugepages) -gt 0 ]
+    then
+        LARGE_PAGES="--large-pages"
+    fi
+fi
+
+HARD_AES=""
+if [ $(grep aes /proc/cpuinfo | wc -c) -ne 0 ]
+then
+  HARD_AES="--hard-aes"
+fi
+
+#ethminer --noeval $LARGE_PAGES $HARD_AES $@

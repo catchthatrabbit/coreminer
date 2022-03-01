25799c2ec4d4fe1d7150dba0e02fc2b5d85c428e
Rastislav <rastislav@wallit.eu>
2022-03-01 22:46:55 +0100

    Fix wallet validation

--- mine.sh
+++ mine.sh
@@ -88,17 +88,25 @@ validate_wallet()
 	BC=$(which bc)
 	if [[ -x "$BC" ]]; then
 		ord() {
-		    LC_CTYPE=C printf '%d' "'$1"
+			LC_CTYPE=C printf '%d' "'$1"
 		}
 		alphabet_pos() {
-		    echo $((`ord $1` - 55))
+			if [ -n "$1" ] && [ "$1" -eq "$1" ] 2>/dev/null; then
+				echo $1
+			else
+				UPPER=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+				echo $((`ord $UPPER` - 55))
+			fi
 		}
-		ICAN=$1
+		ICAN=${1//[[:blank:]]/}
 		COUNTRY=${ICAN:0:2}
 		CHECKSUM=${ICAN:2:2}
 		BCAN=${ICAN:4}
-		COUNTRYSUM=`alphabet_pos ${COUNTRY:0:1}``alphabet_pos ${COUNTRY:1}`
-		OPERAND=`echo $BCAN``echo $COUNTRYSUM``echo $CHECKSUM`
+		BCCO=`echo $BCAN``echo $COUNTRY`
+		for ((i=0; i<${#BCCO}; i++)); do
+			SUM+=`alphabet_pos ${BCCO:$i:1}`
+		done
+		OPERAND=`echo $SUM``echo $CHECKSUM`
 		if [[ `echo "$OPERAND % 97" | $BC` -ne 1 ]]; then
 			echo "Invalid wallet!"
 			exit 1

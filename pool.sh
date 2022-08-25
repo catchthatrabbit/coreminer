19e4f40aec07e30da940513d41a1cc32a15a8292
Sasha Zezulinsky <alexander.zezulinsky@gmail.com>
2022-08-25 23:52:14 +0200

    retab

--- pool.sh
+++ pool.sh
@@ -14,73 +14,73 @@
 
 start_mining()
 {
-	SECURE_JIT=""
-	cOS=$(uname -a | awk '{print $(1)}')
-	cPLT=$(uname -a | awk '{print $(NF)}')
-	if [ "$cOS" == "Darwin" ] && [ "$cPLT" == "arm64" ]; then
-		SECURE_JIT="--jit-secure"
-	fi
+    SECURE_JIT=""
+    cOS=$(uname -a | awk '{print $(1)}')
+    cPLT=$(uname -a | awk '{print $(NF)}')
+    if [ "$cOS" == "Darwin" ] && [ "$cPLT" == "arm64" ]; then
+        SECURE_JIT="--jit-secure"
+    fi
 
-	LARGE_PAGES=""
-	if [ -f /proc/sys/vm/nr_hugepages ]; then
-			if [ $(cat /proc/sys/vm/nr_hugepages) -gt 0 ]; then
-					LARGE_PAGES="--large-pages"
-			fi
-	fi
+    LARGE_PAGES=""
+    if [ -f /proc/sys/vm/nr_hugepages ]; then
+            if [ $(cat /proc/sys/vm/nr_hugepages) -gt 0 ]; then
+                    LARGE_PAGES="--large-pages"
+            fi
+    fi
 
-	HARD_AES=""
-	if [ $(grep aes /proc/cpuinfo 2>&1 | wc -c) -ne 0 ];	then
-		HARD_AES="--hard-aes"
-	fi
+    HARD_AES=""
+    if [ $(grep aes /proc/cpuinfo 2>&1 | wc -c) -ne 0 ];    then
+        HARD_AES="--hard-aes"
+    fi
 
-	POOLS=""
-	for pool in "${@:1}"
-	do
-		POOLS+="-P ${pool} "
-	done
+    POOLS=""
+    for pool in "${@:1}"
+    do
+        POOLS+="-P ${pool} "
+    done
 
-	coreminer --noeval $LARGE_PAGES $HARD_AES $SECURE_JIT $API $POOLS $THREADS
+    coreminer --noeval $LARGE_PAGES $HARD_AES $SECURE_JIT $API $POOLS $THREADS
 }
 
 validate_wallet()
 {
-	BC=$(which bc)
-	if [[ -x "$BC" ]]; then
-		ord() {
-			LC_CTYPE=C printf '%d' "'$1"
-		}
-		alphabet_pos() {
-			if [ -n "$1" ] && [ "$1" -eq "$1" ] 2>/dev/null; then
-				echo $1
-			else
-				UPPER=$(echo "$1" | tr '[:lower:]' '[:upper:]')
-				echo $((`ord $UPPER` - 55))
-			fi
-		}
-		ICAN=$1
-		COUNTRY=${ICAN:0:2}
-		CHECKSUM=${ICAN:2:2}
-		BCAN=${ICAN:4}
-		BCCO=`echo $BCAN``echo $COUNTRY`
-		SUM=""
-		for ((i=0; i<${#BCCO}; i++)); do
-			SUM+=`alphabet_pos ${BCCO:$i:1}`
-		done
-		OPERAND=`echo $SUM``echo $CHECKSUM`
-		if [[ `echo "$OPERAND % 97" | $BC` -ne 1 ]]; then
-			exit 2
-		fi
-	fi
+    BC=$(which bc)
+    if [[ -x "$BC" ]]; then
+        ord() {
+            LC_CTYPE=C printf '%d' "'$1"
+        }
+        alphabet_pos() {
+            if [ -n "$1" ] && [ "$1" -eq "$1" ] 2>/dev/null; then
+                echo $1
+            else
+                UPPER=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+                echo $((`ord $UPPER` - 55))
+            fi
+        }
+        ICAN=$1
+        COUNTRY=${ICAN:0:2}
+        CHECKSUM=${ICAN:2:2}
+        BCAN=${ICAN:4}
+        BCCO=`echo $BCAN``echo $COUNTRY`
+        SUM=""
+        for ((i=0; i<${#BCCO}; i++)); do
+            SUM+=`alphabet_pos ${BCCO:$i:1}`
+        done
+        OPERAND=`echo $SUM``echo $CHECKSUM`
+        if [[ `echo "$OPERAND % 97" | $BC` -ne 1 ]]; then
+            exit 2
+        fi
+    fi
 }
 
 compose_stratum()
 {
-	# scheme://wallet[.workername][:password]@hostname:port[/...]
-	if [[ -z "$3" ]]; then
-		echo "stratum1+tcp://$1@$2"
-	else
-		echo "stratum1+tcp://$1.$3@$2"
-	fi
+    # scheme://wallet[.workername][:password]@hostname:port[/...]
+    if [[ -z "$3" ]]; then
+        echo "stratum1+tcp://$1@$2"
+    else
+        echo "stratum1+tcp://$1.$3@$2"
+    fi
 }
 
 while [ $# -gt 0 ]; do
@@ -112,16 +112,16 @@ while [ $# -gt 0 ]; do
 done
 
 if [[ -z "$WALLET" ]]; then
-	exit 1
+    exit 1
 else
-	ICANWALLET=${WALLET//[[:blank:]]/}
+    ICANWALLET=${WALLET//[[:blank:]]/}
 fi
 
 validate_wallet $ICANWALLET
 
 if [[ -z "$WORKER" ]]; then
-	RAND=$(( ((RANDOM<<15)|RANDOM) % 63001 + 2000 ))
-	WORKER="worker-$RAND"
+    RAND=$(( ((RANDOM<<15)|RANDOM) % 63001 + 2000 ))
+    WORKER="worker-$RAND"
 fi
 
 STRATUM=""
